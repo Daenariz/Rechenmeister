1f68b00fd67fda289689e29a157f0d6cf7d6dc27
Daenariz <deniz.onlinegames@gmail.com>
2024-06-03 14:09:15 +0200

    v1

---
+++ app/src/main/java/com/example/rechenmeister/AddAct.kt
@@ -0,0 +1,71 @@
+package com.example.rechenmeister
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+
+class AddAct : AppCompatActivity(), View.OnClickListener{
+
+    lateinit var  sharedPreferences: SharedPreferences
+    lateinit var inputBetrag : EditText
+    lateinit var btnConfirm : Button
+    lateinit var btnPerson : ToggleButton
+    //lateinit var btnPerson2 : Button
+    //lateinit var btnBar : Button
+    //lateinit var btnCard : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        inputBetrag = findViewById(R.id.input_hint)
+        btnConfirm = findViewById(R.id.btn_confirm)
+        btnPerson = findViewById(R.id.btn_person)
+
+        btnConfirm.setOnClickListener {
+            val value = inputBetrag
+            saveValue(value.toString().toFloat())
+            //finish() // Zurück zur vorherigen Activity
+            Log.v("DEMO","onClickListener aufgerufen mit Wert $value")
+        }
+        //btnPerson.text = "Nana"
+
+        //btnPerson1 = findViewById(R.id.btn_person1)
+        //btnPerson2 = findViewById(R.id.btn_person2)
+        //btnBar = findViewById(R.id.btn_bar)
+
+    }
+
+    override fun onClick(v: View?) {
+
+
+        /*when(v?.id) {
+            R.id.btn_confirm -> {
+                val value = inputBetrag.text.toString()
+                saveValue(value)
+                finish()
+                Log.v("DEMO","OnClick confirmButton aufgerufen")
+            }
+            R.id.btn_person -> {
+
+                Log.v("DEMO","OnClick Toggle1 aufgerufen")
+            }
+        }*/
+    }
+
+    private fun saveValue(value: Float){
+        val editor = sharedPreferences.edit()
+        editor.putFloat("value",value)
+        editor.apply()
+    }
+}
---
+++ app/src/main/java/com/example/rechenmeister/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.rechenmeister
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var savedValue : String
+    lateinit var btnAdd : Button
+    lateinit var btnStats : Button
+    lateinit var btnShare : Button
+
+    lateinit var clause : TextView
+    lateinit var resultTv : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        savedValue = sharedPreferences.getString("value", "").toString()
+
+        btnAdd = findViewById(R.id.btn_add)
+        btnStats = findViewById(R.id.btn_stats)
+        btnShare = findViewById(R.id.btn_share)
+
+        clause = findViewById(R.id.clause)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnStats.setOnClickListener(this)
+        btnShare.setOnClickListener(this)
+
+        resultTv.text = "Gespeicherter Wert: $savedValue"
+        }
+
+    override fun onResume() {
+
+        super.onResume()
+        savedValue = sharedPreferences.getString("value", "").toString()
+        resultTv.text = "Gespeicherter Wert: $savedValue"
+
+        Log.v("DEMO", "onResume wird ausgeführt")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("DEMO", "onPause wird ausgeführt")
+    }
+
+
+    override fun onClick(v: View?) {
+        clause.text = "Dankeschön"
+        var result = 0.0
+        when(v?.id){
+            R.id.btn_add ->{
+                val intent = Intent(this,AddAct::class.java)
+                startActivity(intent)
+                Log.v("DEMO","onClick addButton aufgerufen")
+            }
+            R.id.btn_stats ->{
+                //result = b
+            }
+            R.id.btn_share ->{
+                //result = b
+            }
+        }
+    }
+}
